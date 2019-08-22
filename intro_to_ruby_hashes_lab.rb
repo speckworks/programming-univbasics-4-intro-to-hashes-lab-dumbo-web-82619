7e76591cee0f8671654bdfc6e7e8128a348dbcd4
John Karrell Speck <speckworks@gmail.com>
2019-08-22 12:53:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,9 +29,18 @@ end
 def update_counting_hash(hash, key)
   if hash[key]
     hash[key] = hash[key] += 1
-    end
+  else
   hash[key] = 1
   end
 hash
+
+def update_counting_hash(hash, key)
+  if hash[key]
+     hash[key] = hash[key] +=1
+  
+   hash[key] = 1
+end
+hash
+end
 
 
