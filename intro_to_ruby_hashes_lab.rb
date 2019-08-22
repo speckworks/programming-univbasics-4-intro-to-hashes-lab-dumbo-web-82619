3fbb8b5f2426414f4a4a9cb187f279696edd15b1
John Karrell Speck <speckworks@gmail.com>
2019-08-22 12:36:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   counting = Hash.new(key)
   hash[key] = 1
-  if hash(key) == nil
+  if hash[key] == nil
     hash[key] = 1
   else hash[key] += 1
   end
