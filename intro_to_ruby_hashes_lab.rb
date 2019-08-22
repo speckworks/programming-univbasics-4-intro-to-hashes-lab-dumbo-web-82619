f0ab3d5b657ccc1579cbc9ca07d19d4bbb2115f9
John Karrell Speck <speckworks@gmail.com>
2019-08-22 12:53:36 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,4 @@
+require "pry"
 def new_hash
   {}
 end
@@ -7,7 +8,7 @@ def my_hash
 end
 
 def pioneer
-  pioneer = {:id, Grace Hopper}
+  pioneer = {:name => 'Grace Hopper'}
 end
 
 def id_generator
@@ -26,12 +27,13 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
   if hash[key]
-  hash[key] += 1
-else
-  puts "Key not found!"
+    hash[key] = hash[key] += 1
+  else
+  hash[key] = 1
+  end
+hash
 end
-end
+
+
+
